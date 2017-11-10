cc2b77cfe347f07d9deff215e9a1bfa985c97c43
jing cheng <xinjingcheng@outlook.com>
2017-11-10 22:23:24 +0800

    加入search

--- examples/components/Search.vue
+++ examples/components/Search.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <Search v-on:search="search" v-model="serarchText"></Search>
+    <br>
+    <br>
+    <br>
+    <TitleCell>禁止输入的搜索</TitleCell>
+    <br>
+    <Search v-on:click="readonlySearch"></Search>
   </div>
 </template>
 <script>
@@ -16,6 +22,10 @@ export default {
       if (this.serarchText) {
         this.$msg.info('搜索内容:' + this.serarchText)
       }
+    },
+    readonlySearch() {
+      //禁止输入的搜索
+      this.$msg.info('禁止输入的搜索')
     }
   }
 }
